913c1647a4038cca8fe04b5df397b5c2170a5981
Scott T Lyman <s.t.lyman84@gmail.com>
2022-01-25 12:40:21 -0700

    practice

--- app.js
+++ app.js
@@ -17,10 +17,38 @@ const cases = [
 ]
 
 // YOUR JS CODE
-var currentYear = "2022";
-var jsReleaseYear = "1995";
-let jsAge = currentYear - jsReleaseYear;
+var currentYear = "2022"
+var jsReleaseYear = "1995"
+let jsAge = currentYear - jsReleaseYear
 console.log("current age of JavaScript is " + jsAge)
+if(jsAge >= ageToVote){
+  console.log("JavaScript is old enough to vote")
+} else if(jsAge < ageToVote){
+  console.log("JavaScript is not old enough to vote")
+}
+if(jsAge >= (ageToVote + "82")){
+  console.log("JavaScript is old enough to vote")
+} else if(jsAge < (ageToVote + "82")){
+  console.log("JavaScript is not old enough to vote")
+}
 
+console.log("Starting")
+for (let i = 0; i <= 100;i++){
+  console.log (i)
+}
+console.log("Starting")
+for (let i = 0; i <= 100;i += 10){
+  if (i === 50) {
+    console.log(i + " Halfway")
+  } else if (i === 100) {
+    console.log(i + " Finished")
+  } else {
+  console.log (i)
+}
+}
 
+console.log(user.name + " lives at " + user.address.street + ", " + user.address.city)
 
+for (i = 0; i < cases.length; i++) {
+  console.log (cases[i].title + " , " + cases[i].releaseYear);
+}
